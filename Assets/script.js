e17f7134fc6616abb6c720e5402154b3be03b46e
saadyousafi87 <saad.yousafi87@gmail.com>
2022-09-24 12:14:44 -0400

    added functions to get password length and generate password

--- Assets/script.js
+++ Assets/script.js
@@ -9,5 +9,23 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// function to generate password
+function generatePassword(){
+  var length = passwordLength();
+ 
+  console.log(length);
+}
+
+// fucntion to get password length
+function passwordLength(){
+  var passLength;
+  do {
+     passLength = parseInt(window.prompt("Please choose password length between 8 and 128"));
+  } while (passLength < 8 || passLength > 128);
+  return passLength;
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
