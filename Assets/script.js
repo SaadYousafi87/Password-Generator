e83b33d33e9f5747e5ae211059ae742de7a9908c
saadyousafi87 <saad.yousafi87@gmail.com>
2022-09-24 12:17:23 -0400

    added function to get uppercase or lowercase character

--- Assets/script.js
+++ Assets/script.js
@@ -12,8 +12,10 @@ function writePassword() {
 // function to generate password
 function generatePassword(){
   var length = passwordLength();
- 
+  var upperLower = getUpperLower();
+
   console.log(length);
+  console.log(upperLower);
 }
 
 // fucntion to get password length
@@ -23,6 +25,19 @@ function passwordLength(){
      passLength = parseInt(window.prompt("Please choose password length between 8 and 128"));
   } while (passLength < 8 || passLength > 128);
   return passLength;
+}
+
+// function to get uppercase or lowercase character
+function getUpperLower(){
+  var caseLetter; 
+  do{
+    var bool = false;
+    caseLetter = window.prompt("Please enter the uppercase or lowercase character");
+    if(!isNaN(caseLetter)){
+      bool = true;
+    }
+  } while (bool);
+  return caseLetter;
 }
 
 
